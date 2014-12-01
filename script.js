420074ee534daa490fa282d416fcb97f1be38189
Caleb Brewer <calebthebrewer@gmail.com>
2014-12-01 17:16:22 -0600

    adding Nodes service to wrap local storage

--- script.js
+++ script.js
@@ -1,138 +1,173 @@
 angular
 	.module('breakdown', [])
-	.directive('node', function() {
-		return {
-			scope: {
-				node: '=data'
-			},
-			templateUrl: 'node.html',
-			link: function($scope) {
+	.directive('node', [
+		'Nodes',
+		function (Nodes) {
+			return {
+				scope: {
+					node: '=data'
+				},
+				templateUrl: 'node.html',
+				link: function ($scope) {
 
-				var parent,
-					$node = $scope.node,
-					children = angular.copy($node.children) || [];
+					var parent,
+						$node = $scope.node,
+						children = angular.copy($node.children) || [];
 
-				//properties are inherited from parents prototype
-				if (!$node.properties && $node.parent) {
-					parent = JSON.parse(localStorage.getItem('node.' + $node.parent));
-					$node.properties = parent.prototype;
-				} else if (!$node.properties) {
-					$node.properties = {};
-				}
-
-				//the prototype is modified locally
-				$node.prototype = $node.prototype || {};
-
-				//load children
-				loadChildren();
-
-				save();
-
-				$scope.$on('remove', function(event, childId) {
-					if (childId === $node.id) return;
-
-					remove(true);
-				});
-
-				$scope.$on('remove-child', function(event, childId) {
-					var childIndex = children.indexOf(childId);
-					if (childIndex > 0) {
-						children.splice(childIndex, 1);
-						save();
-						loadChildren();
+					//properties are inherited from parents prototype
+					if (!$node.properties && $node.parent) {
+						parent = Nodes.get($node.parent);
+						$node.properties = parent.prototype;
+					} else if (!$node.properties) {
+						$node.properties = {};
 					}
-				});
 
-				$scope.addPrototype = function() {
-					$node.prototype[$scope.property] = $scope.value;
-					$scope.property = '';
-					$scope.value = '';
+					//the prototype is modified locally
+					$node.prototype = $node.prototype || {};
 
-					save();
-				};
+					//load children
+					loadChildren();
 
-				$scope.removePrototype = function(key) {
-					delete $node.prototype[key];
+					$scope.$on('remove', function (event, childId) {
+						//if I triggered the remove, don't make me do it again
+						if (childId === $node.id) return;
 
-					save();
-				};
+						remove();
+					});
 
-				$scope.addChild = function() {
-					var child = {
-						id: ($node.id || 0) + '.' + $node.children.length,
-						label: 'Child ' + $node.children.length,
-						properties: $node.prototype,
-						parent: $node.id
+					$scope.$on('remove-child', function (event, childId) {
+						var childIndex = children.indexOf(childId);
+						if (childIndex > 0) {
+							children.splice(childIndex, 1);
+							save();
+							//slice it out of the scope manually
+							for (var i = 0, l = $node.children.length; i < l; i++) {
+								if ($node.children[i].id === childId) {
+									$node.children.splice(i, 1);
+								}
+							}
+						}
+					});
+
+					$scope.addPrototype = function () {
+						$node.prototype[$scope.property] = $scope.value;
+						$scope.property = '';
+						$scope.value = '';
+
+						save();
 					};
 
-					children.push(child.id);
-					save();
-					addChild(child);
-				};
+					$scope.removePrototype = function (key) {
+						delete $node.prototype[key];
 
-				$scope.remove = remove;
+						save();
+					};
 
-				function addChild(child) {
-					if (typeof child !== 'object') {
-						child = JSON.parse(localStorage.getItem('node.' + child));
-						if (child === null) {
-							return false;
-						}
-						child.properties = $node.prototype;
-					}
+					$scope.addChild = function () {
+						var child = {
+							id: ($node.id || 0) + '.' + $node.children.length,
+							label: 'Child ' + $node.children.length,
+							properties: $node.prototype,
+							parent: $node.id
+						};
 
-					$node.children.push(child);
-					return true;
-				}
+						children.push(child.id);
+						save();
+						addChild(child);
+					};
 
-				function save() {
-					localStorage.setItem('node.' + $node.id, JSON.stringify({
-						id: $node.id,
-						label: $node.label,
-						prototype: $node.prototype,
-						children: children
-					}));
-
-					$scope.$emit('change');
-				}
-
-				function remove(dontEmit) {
-					if (localStorage.getItem('node.' + $node.id)) {
-						localStorage.removeItem('node.' + $node.id);
+					$scope.remove = function () {
+						//first tell all your friends to remove
 						$scope.$broadcast('remove', $node.id);
-					}
-					if (!dontEmit) {
 						$scope.$emit('remove-child', $node.id);
-					}
-				}
 
-				function loadChildren() {
-					$scope.node.children = [];
-					for (var i = 0, l = children.length; i < l; i++) {
-						if (!addChild(children[i])) {
-							children.splice(i, 1);
-							save();
+						remove();
+					};
+
+
+					function addChild(child) {
+						if (typeof child !== 'object') {
+							child = Nodes.get(child);
+							if (child === null) {
+								return false;
+							}
+							child.properties = $node.prototype;
+						}
+
+						$node.children.push(child);
+						return true;
+					}
+
+					function save() {
+						Nodes.save($node.id, {
+							id: $node.id,
+							label: $node.label,
+							prototype: $node.prototype,
+							children: children
+						});
+					}
+
+					function remove() {
+						if (Nodes.get($node.id)) {
+							Nodes.remove($node.id);
+						}
+					}
+
+					function loadChildren() {
+						$scope.node.children = [];
+						for (var i = 0, l = children.length; i < l; i++) {
+							if (!addChild(children[i])) {
+								children.splice(i, 1);
+								save();
+							}
 						}
 					}
 				}
+			};
+		}])
+	.service('Nodes', function () {
+		var prefix = 'node.';
+
+		this.save = function (id, node) {
+			var string = JSON.stringify(node);
+			localStorage.setItem(prefix + id, string);
+		};
+
+		this.get = function (id) {
+			var string = localStorage.getItem(prefix + id);
+			if (string) {
+				return JSON.parse(string);
+			} else {
+				return false;
 			}
 		};
-	})
-	.controller('home', ['$scope', function($scope) {
 
-		var node;
-		try {
-			node = JSON.parse(localStorage.getItem('node.0'));
-		} catch (e) {
-		}
-
-		$scope.node = node || {
-			id: 0,
-			label: 'Bob',
-			children: []
+		this.remove = function (id) {
+			localStorage.removeItem(prefix + id);
 		};
 
-		$scope.flush = function() {
+		this.flush = function () {
 			localStorage.clear();
 		};
-	}]);
+	})
+	.controller('home', [
+		'$scope',
+		'Nodes',
+		function ($scope, Nodes) {
+
+			var node;
+			try {
+				node = Nodes.get('node.0');
+			} catch (e) {
+			}
+
+			$scope.node = node || {
+				id: 0,
+				label: 'Bob',
+				children: []
+			};
+
+			$scope.flush = function () {
+				Nodes.flush();
+			};
+		}]);
